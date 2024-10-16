545d11b3200fd7d384e0b5a33a2a44319af2f9f7
Impk1103 <impk1103@gmail.com>
2024-10-16 15:22:35 +0530

    End of SQL and Sequelize
    changing my databases to NoSQL.

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
     description: description
-  }).then(result => {
+  })
+  .then(result => {
     console.log('Created new product');
     res.redirect('../admin/products');
   }).catch(err => console.log(err));
@@ -30,7 +31,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then(product => {
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+  .then(products => {
+    const product = products[0];
     if(!product){
       return res.redirect('/');
     }
@@ -66,7 +70,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then(products => {
+  req.user.getProducts().then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
